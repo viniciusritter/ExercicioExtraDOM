b4a97d025e2c171505d1e444ff5e5c82e4b89c78
Vinícius Casanova Ritter <viniciuscasanovaritter@hotmail.com>
2022-04-24 17:34:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const imageArray = [
+    "egon1.jpg",
+    "egon2.jpg",
+    "egon3.jpg",
+    "egon4.jpg",
+    "egon5.jpg",
+    "egon6.jpg",
+    "egon7.jpg",
+    "egon8.jpg",
+    "egon9.jpg",
+    "egon10.jpg",
+    "egon11.jpg",
+    "egon12.jpg",
+    "egon13.jpg",
+    "egon14.jpg",
+    "egon15.jpg"
+];
+
+//var numberIndex = Math.floor(Math.random() * imageArray.length);
+const container = document.querySelector('.container');
+
+function getRandomNr(){
+    return Math.floor(Math.random() * imageArray.length);
+}
+
+
+
+function loadImages(numImages = 15)
+{
+    let i = 0;
+    while (i < numImages){
+        const img = document.createElement('img');
+        img.src = `./imagens/${imageArray[getRandomNr()]}`;
+        container.appendChild(img);
+        i++
+    }   
+      
+};
+
+loadImages();
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+       loadImages();
+    }
+})
